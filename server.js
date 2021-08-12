779ccebcf594b90ac8dc2de656420b05eb51c018
ExplorerMarco <marco@explore-ai.net>
2021-08-12 06:49:49 +0200

    fix: server.js build script

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // Install express server 
 const express = require('express');
 const path = require('path');
-const app = express();// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Iota-Portal'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/Iota-Portal/index.html'));
-});// Start the app by listening on the default Heroku port
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/Iota-Portal'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/ExploreInsightsAngular/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
