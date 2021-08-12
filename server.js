e9b9759a95677e38565dd3bf0d5caf8617173c61
ExplorerMarco <marco@explore-ai.net>
2021-08-12 07:31:25 +0200

    fix: deployment issues

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/iota-portal'));
+app.use(express.static('./dist/Iota-Portal'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/iota-portal/'}),
+    res.sendFile('index.html', {root: 'dist/Iota-Portal/'}),
 );
 
 // Start the app by listening on the default Heroku port
