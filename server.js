479e99dbe09b4a42c0a0ade43e89161b5800671c
ExplorerMarco <marco@explore-ai.net>
2021-08-12 06:59:56 +0200

    fix: server.js build script

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('./dist/Iota-Portal'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/ExploreInsightsAngular/'}),
+    res.sendFile('index.html', {root: 'dist/Iota-Portal/'}),
 );
 
 // Start the app by listening on the default Heroku port
